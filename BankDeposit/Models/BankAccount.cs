47710519604d809d20b055732daa2d9f27c5d759
vladyslav28 <164999850+vladyslav28@users.noreply.github.com>
2024-05-21 00:14:33 +0300

    Add project files.

---
+++ BankDeposit/Models/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDeposit.Models
+{
+    public class BankAccount
+    {
+        public int Id { get; set; }
+        public string DepositCategory { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public decimal CurrentAmount { get; set; }
+        public DateTime LastOperationDate { get; set; }
+
+
+        public BankAccount() { }
+    }
+}
---
+++ BankDeposit/Program.cs
@@ -0,0 +1,19 @@
+using BankDeposit.Forms;
+
+namespace BankDeposit
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}
